f4e29a266d8518bf43b6f9a1ac5942a196cfbe37
Apple <apple@renadas-mac.local>
2021-11-07 13:33:57 +0400

    edits

--- SOB31/broken_grades.py
+++ SOB31/broken_grades.py
@@ -21,36 +21,37 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: "))
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: "))
 
-grades = [exam_one exam_two exam_three]
+grades = [exam_one , exam_two , exam_three]
 sum = 0
-for grade in grade:
+for grade in grades:
   sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades)
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90:
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
+    letter_grade = "C"
+elif avg > 59 and avg < 70:
     letter_grade = "D"
-elif:
+else:
     letter_grade = "F"
 
 for grade in grades:
     print("Exam: " + str(grade))
 
-    print("Average: " + str(avg))
+    
+print("Average: " + str(avg))
 
-    print("Grade: " + letter_grade)
+print("Grade: " + letter_grade)
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F":
+    print ("Student is failing.")
 else:
-    print "Student is passing."
+    print ("Student is passing.")

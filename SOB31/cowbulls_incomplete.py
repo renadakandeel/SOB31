81f7c76ef41b9b75b2f8df97497228eed167dc82
Apple <apple@renadas-mac.local>
2021-11-17 20:20:26 +0400

    fixed the code for cowbulls.py file

--- SOB31/cowbulls_incomplete.py
+++ SOB31/cowbulls_incomplete.py
@@ -2,12 +2,20 @@ import random
 
 def compare_numbers(number, user_guess):
     ## your code here
+    cowbull = [0, 0]
+    user_guess_str = str(user_guess)
+    for r in range(4):
+        if (user_guess_str[r] == number[r]):
+            cowbull[1] +=1
+        else:
+            cowbull[0] += 1
     return cowbull
 
 playing = True #gotta play the game
 number = str(random.randint(0,9999)) #random 4 digit number
 guesses = 0
-print number
+
+print (number)
 
 print("Let's play a game of Cowbull!") #explanation
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
@@ -16,7 +24,7 @@ print("The game ends when you get 4 bulls!")
 print("Type exit at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
+    user_guess = input("Give me your best guess!")
     if user_guess == "exit":
         break
     cowbullcount = compare_numbers(number,user_guess)
